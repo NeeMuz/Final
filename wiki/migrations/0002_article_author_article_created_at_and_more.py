9d4d3a2e772286bab5be7b95368f1af612ed4f31
DDreydyy <66523537+NeeMuz@users.noreply.github.com>
2026-09-01 16:56:34 +0200

    Save project

--- wiki/migrations/0002_article_author_article_created_at_and_more.py
+++ wiki/migrations/0002_article_author_article_created_at_and_more.py
@@ -6,18 +6,27 @@ from django.utils.text import slugify
 
 
 def forwards(apps, schema_editor):
-    Article = apps.get_model('wiki', 'Article')
+    Article = apps.get_model("wiki", "Article")
+
     for article in Article.objects.all():
         if not article.slug:
-            base = slugify(article.title) or f'article-{article.pk}'
+            base = slugify(article.title) or f"article-{article.pk}"
             slug = base
             counter = 1
+
             while Article.objects.filter(slug=slug).exclude(pk=article.pk).exists():
-                slug = f'{base}-{counter}'
+                slug = f"{base}-{counter}"
                 counter += 1
+
             article.slug = slug
+
         if not article.created_at:
-            article.created_at = getattr(article, 'creation_date', None) or timezone.now()
+            article.created_at = getattr(
+                article,
+                "creation_date",
+                None,
+            ) or timezone.now()
+
         article.save()
 
 
@@ -25,59 +34,68 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('wiki', '0001_initial'),
+        ("wiki", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='article',
-            name='author',
+            model_name="article",
+            name="author",
             field=models.ForeignKey(
                 blank=True,
                 null=True,
                 on_delete=django.db.models.deletion.SET_NULL,
-                related_name='articles',
+                related_name="articles",
                 to=settings.AUTH_USER_MODEL,
             ),
         ),
+
         migrations.AddField(
-            model_name='article',
-            name='created_at',
+            model_name="article",
+            name="created_at",
             field=models.DateTimeField(default=timezone.now),
         ),
+
         migrations.AddField(
-            model_name='article',
-            name='slug',
-            field=models.SlugField(blank=True, max_length=220),
+            model_name="article",
+            name="slug",
+            field=models.SlugField(
+                blank=True,
+                max_length=220,
+            ),
         ),
+
         migrations.AddField(
-            model_name='article',
-            name='updated_at',
+            model_name="article",
+            name="updated_at",
             field=models.DateTimeField(default=timezone.now),
         ),
+
         migrations.AlterField(
-            model_name='article',
-            name='title',
+            model_name="article",
+            name="title",
             field=models.CharField(max_length=200),
         ),
-        migrations.RunPython(forwards, migrations.RunPython.noop),
+
+        migrations.RunPython(
+            forwards,
+            migrations.RunPython.noop,
+        ),
+
         migrations.AlterField(
-            model_name='article',
-            name='created_at',
+            model_name="article",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True),
         ),
+
         migrations.AlterField(
-            model_name='article',
-            name='slug',
-            field=models.SlugField(blank=True, max_length=220, unique=True),
-        ),
-        migrations.AlterField(
-            model_name='article',
-            name='updated_at',
+            model_name="article",
+            name="updated_at",
             field=models.DateTimeField(auto_now=True),
         ),
+
         migrations.RemoveField(
-            model_name='article',
-            name='creation_date',
+            model_name="article",
+            name="creation_date",
         ),
     ]
